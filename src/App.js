cf7c3b8b6b728651249fec0fbef67ec58207aa36
josephchimebuka <chimebujoseph@gmail.com>
2022-09-24 02:05:16 +0100

    Completed landing animations

--- src/App.js
+++ src/App.js
@@ -15,15 +15,27 @@ function App() {
     //timeline
     let tl = gsap.timeline();
 
-    tl.from(".line span",1.8,{
-     y:100,
-     ease:"Power4.Out",
-     delay:2,
-     skew:7,
-     stagger:{
-      amount:0.3
-     }
-    });
+    // tl.from(".line span", 1.8,{
+    //  y:90,
+    //  ease:"power4.out",
+    //  delay:1,
+    //  skewY:8,
+    //  stagger:{
+    //   amount:0.3
+    //  }
+    // })
+    tl.to(".overlay-top", 1.6,{
+      height:0,
+      ease: "expo.inOut",
+      stagger:0.4
+    }).to(".overlay-bottom", 1.6,{
+      width:0,
+      delay:-.8,
+      ease:"expo.inOut",
+      stagger:{
+        amount:0.4,
+      }
+    }).to(".introOverlay",0,{css:{display:"none"}})
   },[]);
   return (
     <div className="App">
